df761d968b1df537a581a8acf57ce7c33c3bc4e5
David Stenszky <krunpli@gmail.com>
2019-05-13 03:59:55 +0200

    Add files via upload

--- proofingBox.py
+++ proofingBox.py
@@ -1,57 +1,80 @@
 # encoding: utf-8
-
 from OmegaExpansion import oledExp
-import time, os
+import math, time, os
 
-# initialisation
+
+## initialisation
+print('Starting the program proofingBox...')
+
+# initialisation the oled expansion
 lcd  = oledExp.driverInit()
 if lcd != 0:
-	print('Error initalizing OLED Explansion')
-
-# kell ez???
+	print('Error initalizing OLED Expansion')
+# set verbosity
 oledExp.setVerbosity(0)
-#lcd     = oledExp.setTextColumns()
-
-# set up the display (show an image for 10 seconds)
-imgBoot     = "/home/proofingBox/initImg.lcd"
-if os.path.exists(imgBoot):
-	lcd = oledExp.drawFromFile(imgBoot)
-time.sleep(1)
-#lcd		= oledExp.clear()
-
-# get target and actual temperature values
-tempTrg    = "25 °C"
-tempAct    = "3.14 °C"
-
 # set the brightness
 lcd     = oledExp.setBrightness(127)
+if lcd != 0:
+	print('Error setting the brightness for the OLED Expansion')
 
-# set up the temperature block
-# here comes a pic with a thermometer
-#imgTemp = oledExp.setImageColumns(0,0)
-#imgTemp = "/home/proofingBox/thermometer-icon.lcd"
-#if os.path.exists(imgTemp):
-	#lcd = oledExp.drawFromFile(imgTemp)
-#else: lcd = oledExp.write("No image")
-# here comes the target temperature value
-#lcd     = oledExp.setCursor(1, 0)
-#lcd     = oledExp.setCulomnAdressing(33, 40)
-lcd     = oledExp.write("Target:")
+# set up the boot display (show an image for 10 seconds)
+imgBoot     = "/home/proofingBox/initImg.lcd"
+if os.path.exists(imgBoot):
+	oledExp.drawFromFile(imgBoot)
 time.sleep(1)
-#lcd     = oledExp.setCulomnAdressing(88, 124)
-lcd     = oledExp.write(tempTrg)
-time.sleep(1)
-# here comes the actual temperature value
-#lcd     = oledExp.setCursor(3, 0)
-#lcd     = oledExp.setCulomnAdressing(33, 40)
-lcd     = oledExp.write("Actual:")
-time.sleep(1)
-#lcd     = oledExp.setCulomnAdressing(88, 124)
-lcd     = oledExp.write(tempAct)
+oledExp.clear()
 
-# set up the time block
 
-# set up the heater on/off block
+## get the target and actual temperature values
+tempTrg    = "25"
+tempAct    = "21"
+
+## get the relay status
+
+
+## get the uptime
+def get_uptime():
+    with open('/proc/uptime', 'r') as f:
+        uptimeS = float(f.readline().split()[0])
+    	uptimeMin = uptimeS / (60 * 1.0)
+    	uptimeH = uptimeMin / (60 * 1.0)
+    return uptimeH
+
+## main function
+def init(tempTrg,tempAct):
+	"teszt"
+# here comes the background image (than it won't be overwritten)
+oledExp.setImageColumns(0,0)
+imgBackground = "/home/proofingBox/background.lcd"
+if os.path.exists(imgBackground):
+	oledExp.drawFromFile(imgBackground)
+else: oledExp.write("No background image")
+
+# display the target temperature value
+oledExp.setTextColumns()
+oledExp.setCursor(1,7)
+oledExp.write("Target:   " + tempTrg + " C")
+
+# display the actual temperature value
+oledExp.setTextColumns()
+oledExp.setCursor(3,7)
+oledExp.write("Actual:   " + tempAct + " C")
+
+# display the time gone (actually uptime of the system)
+uptime_h = int(math.modf(get_uptime())[1])
+uptime_min = int(math.modf(get_uptime())[0]*60)
+oledExp.setTextColumns()
+oledExp.setCursor(5,6)
+oledExp.write(str(uptime_h) + " h")
+oledExp.setTextColumns()
+oledExp.setCursor(7,6)
+oledExp.write(str(uptime_min) + " min")
+
+# display the heater on/off block
+oledExp.setTextColumns()
+oledExp.setCursor(6,18)
+oledExp.write("OFF")
+
 
 
 # at the end: clear the display
@@ -63,5 +86,4 @@ lcd     = oledExp.clear()
 # kell még:
 # - hőérzékelő értékét beépíteni
 # - relét (és fűtést) vezérelni
-# - futó időt kijelezni
 # - ha a hőmérő szélsőértékeket mutat, jelezzen (buzzer?)
